868d3189b6012b1f8adf9c58dee77bc4c252cdfb
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-05-22 22:31:56 +0200

    Add a tiny bit of documentation

--- sample/build.rs
+++ sample/build.rs
@@ -1,3 +1,5 @@
 fn main() {
+	/// Calling `versionator_build::build_script` collects all data and makes it available to `versionator::versionator!`
+	/// and `versionator::format!` in the main program.
 	versionator_build::build_script();
 }
--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,7 +1,12 @@
-versionator::versionator!(fn version);
+/// Use the `versionator!` macro to generate a function `crate::build_info` that returns on object with the data that
+/// is collected in the build script.
+versionator::versionator!(fn build_info);
 
 fn main() {
-	println!("{:?}", version());
+	// We can now either use the `build_info` function to work with the collected data at runtime...
+	println!("{:?}", build_info());
+
+	// ... or format it directly to a single `&'static str` at compile time
 	println!(
 		"{}",
 		versionator::format!("{{{.crate_info.name} v{.crate_info.version} built with rustc version {.compiler.version} {.compiler.commit_hash} at {.timestamp}}}")
