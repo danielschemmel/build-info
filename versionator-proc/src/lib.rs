3dac4cd849627079d5ab7385d0318ff6e1740764
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-05-22 22:55:44 +0200

    Add a switch to toggle off the use of the runtime functionaltiy

--- versionator-proc/src/lib.rs
+++ versionator-proc/src/lib.rs
@@ -2,9 +2,11 @@ use proc_macro::TokenStream;
 use proc_macro_hack::proc_macro_hack;
 
 mod format;
+#[cfg(feature = "runtime")]
 mod function;
 mod init_value;
 
+#[cfg(feature = "runtime")]
 #[proc_macro]
 pub fn versionator(input: TokenStream) -> TokenStream {
 	function::versionator(input)
--- versionator/src/lib.rs
+++ versionator/src/lib.rs
@@ -2,7 +2,9 @@ use proc_macro_hack::proc_macro_hack;
 
 pub use versionator_common::*;
 
+#[cfg(feature = "runtime")]
 pub use lazy_static::lazy_static; // used by the proc macro
+#[cfg(feature = "runtime")]
 pub use versionator_proc::versionator;
 
 #[proc_macro_hack]
