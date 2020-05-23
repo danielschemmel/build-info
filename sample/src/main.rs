427c36ea040319f0f77fcbb22edb5179b889e19d
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-05-23 19:27:29 +0200

    rename function created in sample application

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,10 +1,10 @@
 // Use the `versionator!` macro to generate a function `crate::build_info` that returns on object with the data that
 // is collected in the build script.
-versionator::versionator!(fn version);
+versionator::versionator!(fn build_info);
 
 fn main() {
 	// We can now either use the `build_info` function to work with the collected data at runtime...
-	println!("{:?}", version());
+	println!("{:?}", build_info());
 
 	// ... or format it directly to a single `&'static str` at compile time
 	println!(
