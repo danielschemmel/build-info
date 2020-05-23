480a7c025557671d5c5bf4993109668ec36caf3f
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-05-23 11:06:04 +0200

    Document, lint and format

--- versionator-build/src/lib.rs
+++ versionator-build/src/lib.rs
@@ -4,8 +4,9 @@ mod compiler;
 mod crate_info;
 mod version_control;
 
+/// Emits a `cargo:rerun-if-changed` line for each file of the target project.
 fn rebuild_if_project_changes() {
-	println!("cargo:rerun-if-changed={}", "Cargo.toml");
+	println!("cargo:rerun-if-changed=Cargo.toml");
 	for source in glob::glob_with(
 		"**/*.rs",
 		glob::MatchOptions {
@@ -13,13 +14,18 @@ fn rebuild_if_project_changes() {
 			require_literal_separator: false,
 			require_literal_leading_dot: false,
 		},
-	).unwrap() {
-		let source = source.unwrap();
+	)
+	.unwrap()
+	.map(|source| source.unwrap())
+	{
 		println!("cargo:rerun-if-changed={}", source.to_string_lossy());
 	}
 }
 
 pub fn build_script() {
+	// Whenever any `cargo:rerun-if-changed` key is set, the default set is cleared.
+	// Since we will need to emit such keys to trigger rebuilds when the vcs repository changes state,
+	// we also have to emit the customary triggers again, or we will only be rerun in that exact case.
 	rebuild_if_project_changes();
 
 	let crate_info = crate_info::read_manifest();
