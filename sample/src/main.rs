689ba695bec18cd58e9e19538a898c927a83c772
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-05-28 20:35:00 +0200

    Clean up sample

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -11,8 +11,4 @@ fn main() {
 		"{}",
 		versionator::format!("{{{.crate_info.name} v{.crate_info.version} built with rustc version {.compiler.version} {.compiler.commit_hash} at {.timestamp}}}")
 	);
-	println!(
-		"{}",
-		versionator::format!("{.compiler.version.pre.len()} :> {.compiler.version.pre}")
-	);
 }
