8408daeaf6c26b8d6001634adf381193d30d38f8
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-05-28 21:24:14 +0200

    Add info about visibility specifiers to the build_info! macro

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,5 +1,6 @@
 // Use the `build_info!` macro to generate a function `crate::build_info` that returns on object with the data that
 // is collected in the build script.
+// This macro supports visibility-specifiers, like `build_info!(pub fn how_this_crate_was_built)`.
 build_info::build_info!(fn build_info);
 
 fn main() {
