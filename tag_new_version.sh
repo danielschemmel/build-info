b1686352456049ed3de5cbd9c539d1422d139add
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-06-07 14:40:53 +0200

    Add additional sanity checks to version tagging script

--- tag_new_version.sh
+++ tag_new_version.sh
@@ -5,4 +5,7 @@ set -u
 set -o pipefail
 
 cargo clippy -- -D warnings
+cargo test --all-features
+git diff --exit-code  # check if unstaged changes exist
+git diff --cached --exit-code  # check if staged, uncommitted changes exist
 exec cargo workspaces version --all --exact --no-individual-tags --force \*
