cb231880cd0218b5aca646ae61a0f60c343d018e
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-06-10 15:35:10 +0200

    `cargo-workspaces` should no be able to publish all crates correctly

--- publish_new_version.sh
+++ publish_new_version.sh
@@ -8,4 +8,4 @@ cargo clippy -- -D warnings
 cargo test --all-features
 git diff --exit-code  # check if unstaged changes exist
 git diff --cached --exit-code  # check if staged, uncommitted changes exist
-exec cargo workspaces version --all --exact --no-individual-tags --force \*
+exec cargo workspaces publish --all --exact --no-individual-tags --force \*
