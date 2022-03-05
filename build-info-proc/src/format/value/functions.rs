6cb384f0bd85163e287f645f25f24d4d75adaf2e
Daniel Schemmel <daniel@schemmel.net>
2022-03-05 21:52:27 +0000

    clarify note

--- build-info-proc/src/format/value/functions.rs
+++ build-info-proc/src/format/value/functions.rs
@@ -7,6 +7,6 @@ use super::Value;
 pub(crate) fn call_function(name: &str, _args: &[Box<dyn Value>], span: Span) -> Result<Box<dyn Value>> {
 	abort!(span,
 		"Function `{}` cannot be called inside `build_info::format!`", name;
-		note = "No functions are implemented for use in `build_info::format!`, as of now.";
+		note = "No (non-macro) functions are implemented for use in `build_info::format!`, as of now.";
 	)
 }
