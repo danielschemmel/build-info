6c1abfec1aaca47b500550590edc37aa4e1a8de8
Daniel Schemmel <daniel@schemmel.net>
2022-11-01 00:20:30 +0000

    verify msrv when tagging new versions

--- tag_new_version.sh
+++ tag_new_version.sh
@@ -9,4 +9,9 @@ cargo test
 cargo test --all-features
 git diff --exit-code  # check if unstaged changes exist
 git diff --cached --exit-code  # check if staged, uncommitted changes exist
+for x in build-info build-info-build build-info-common build-info-proc ; do
+	pushd $x
+	cargo msrv verify
+	popd
+done
 exec cargo workspaces version --all --exact --no-individual-tags --allow-branch main --force \*
